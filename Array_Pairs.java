96436af9bbbe61f58afd72ab4f0265ab0e3b2908
Yash Mahajan <146362840+yaassshhhhh@users.noreply.github.com>
2025-12-20 22:00:04 +0530

    Add files via upload

---
+++ Array_Pairs.java
@@ -0,0 +1,15 @@
+public class Array_Pairs{
+    public static void ArrayPairs(int numbers[]) {
+        for(int i=0;i<numbers.length;i++){
+            int curr = numbers[i];
+            for(int j=i+1;j<numbers.length;j++){
+                System.out.print("(" +curr+ "," +numbers[j]+ ")");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int numbers[] = {2,4,6,8,10};
+        ArrayPairs(numbers);
+    }
+}
---
+++ Reverse_Array.java
@@ -0,0 +1,24 @@
+public class Reverse_Array {
+    public static void reverseArray(int numbers[]){
+        int start=0,end=numbers.length-1;
+
+        while(start < end){
+
+            int temp = numbers[start];
+            numbers[start] = numbers[end];
+            numbers[end] = temp;
+
+            start ++;
+            end --;
+        } 
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = { 2,4,6,8,10,12,14,16,18,20 };
+
+        reverseArray(numbers);
+        for(int i=0;i<numbers.length;i++){
+            System.out.print(" "+numbers[i]);
+        }
+    }
+}
