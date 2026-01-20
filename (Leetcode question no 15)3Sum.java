7ea05505d632b4ee6987c7ab1278222bfb8db748
Yash Mahajan <146362840+yaassshhhhh@users.noreply.github.com>
2026-01-20 22:52:50 +0530

    Create (Leetcode question no 15)3Sum.java

---
+++ (Leetcode question no 15)3Sum.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        // Step 1: Sort the array
+        Arrays.sort(nums);
+
+        // Step 2: Fix one element
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            // Duplicate fixed element skip
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            // Step 3: Two pointer
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    // Triplet found
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    left++;
+                    right--;
+
+                    // Duplicate left skip
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+
+                    // Duplicate right skip
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
